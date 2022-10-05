74a2e6a0f95dd2eb443475576413b32c4fc4553a
Zapperdax <qa80986@gmail.com>
2022-10-05 17:54:18 +0500

    Adding Authentication and Private Paths

--- BackEnd/src/routes/adminRoute.js
+++ BackEnd/src/routes/adminRoute.js
@@ -5,10 +5,14 @@ const auth = require('../middleware/auth');
 const OTP = require('../models/otpModal');
 const generateOTP = require('../utils/generateOTP');
 const sendOTP = require('../utils/sendOTP');
-
+const bcrypt = require('bcrypt');
+const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 
 router.get('/admin/me', auth, async (req, res)=> {
-    res.send(req.admin);
+    const admin = req.admin;
+    const token = req.token;
+    res.send({admin, token});
 })
 router.post('/adminDetails', async (req, res)=> {
     try{
@@ -47,7 +51,7 @@ router.post('/admin/logoutAll', auth, async (req, res)=> {
     try{
         req.admin.tokens = [];
         await req.admin.save();
-        res.send();
+        res.send(req.admin);
     } catch(err){
         res.status(500).send();
     }
@@ -70,9 +74,13 @@ router.patch('/admin/me', auth, async (req, res)=> {
     }
 });
 
-router.post('/resetPassword', async (req, res)=> {
+router.post('/sendOTP', async (req, res)=> {
     try{
         const email = req.body.email;
+        const admin = await Admin.findOne({email: email});
+        if(!admin){
+            return res.status(400).send({"ERROR:": "No Such Admin"});
+        }
         const otp = await generateOTP();
         await sendOTP(otp, email);
         OTP.findOneAndUpdate({email: email}, {otp: otp}, {upsert: true}, (err, result)=> {
@@ -90,5 +98,63 @@ router.post('/resetPassword', async (req, res)=> {
         res.status(500).send(err.message);
     }
 });
+
+router.post('/getOTP', async (req, res) => {
+    try{
+        const otp = await OTP.findOne({email: req.body.email});
+        if(!otp){
+            return res.status(400).send();
+        }
+        res.send(otp);
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+});
+
+router.post('/changePassword', auth, async (req, res)=> {
+    try{
+        req.admin.password = req.body.password;
+        await req.admin.save();
+        res.send(req.admin);
+    } catch(err){
+        res.status(500).send(err.message);
+    }
+});
+
+router.post('/matchPassword', auth, async (req, res)=> {
+    try{
+        const match = await bcrypt.compare(req.body.password, req.admin.password);
+        if(!match){
+            return res.status(400).send({"ERROR": "Current Password Is Incorrect"});
+        }
+        res.status(200).send();
+    } catch(err){
+        res.status(500).send(err.message)
+    }
+})
+
+router.get('/getToken', auth, async (req, res)=> {
+    try{
+        const token = req.admin.token;
+        if(!token){
+            return res.status(400).send();
+        }
+        res.send(token);
+    } catch(err){
+        console.log(err);
+    }
+})
+
+router.post('/verifyToken', async (req, res)=> {
+    try{
+        const verified = jwt.verify(req.body.token, 'thisiszapperdax');
+        if(!verified){
+            return res.send({"ERROR": "ERROR"})
+        }
+        res.json({"Status": "Matched"}).send();
+    } catch(err){
+        res.status(500).send();
+    }
+})
 
 module.exports = router;
