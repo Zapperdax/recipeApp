385019d3a0b39448cfff57365c6c0d69ea822f39
Zapperdax <qa80986@gmail.com>
2022-10-07 11:39:26 +0500

    Adding user authentication and favorite modal

--- BackEnd/src/models/userModel.js
+++ BackEnd/src/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     userName: {
@@ -8,7 +10,42 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+userSchema.statics.findByCredentials = async(userName, password)=>{
+    const user = await User.findOne({userName});
+    if(!user){
+        throw new Error('No User Found');
     }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+        throw new Error('No User Found');
+    }
+    return user;
+
+}
+
+userSchema.methods.generateAuthToken = async function(){
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString()}, 'thisiszapperdax');
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
 })
 
 const User = mongoose.model('User', userSchema);
--- BackEnd/src/routes/userRoute.js
+++ BackEnd/src/routes/userRoute.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/userModel');
+const Favorite = require('../models/favoriteModel');
+const Recipe = require('../models/recipeModel');
+const userAuth = require('../middleware/userAuth');
 
 router.post('/user', async (req, res)=> {
     try{
         const user = new User(req.body);
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
     } catch(err){
         res.status(500).send(err.message);
     }
 })
 
-router.get('/users', async( req, res)=> {
+router.get('/users', async(req, res)=> {
     try{
         const users = await User.find({});
         if(!users){
@@ -22,6 +26,48 @@ router.get('/users', async( req, res)=> {
     } catch(err){
         res.status(400).send();
     }
+});
+
+router.get('/user/me', userAuth, async(req, res)=> {
+    res.send(req.user);
 })
+
+router.post('/user/login', async (req, res)=> {
+    try{
+        const user = await User.findByCredentials(req.body.userName, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({user, token});
+    } catch(err){
+        res.status(400).send()
+    }
+});
+
+router.post('/user/logoutAll', userAuth, async (req, res)=> {
+    try{
+        req.user.tokens = [];
+        await req.user.save();
+        res.send(req.user);
+    } catch(err){
+        res.status(400).send();
+    }
+});
+
+router.post('/favorite/:id', userAuth, async(req, res)=> {
+    try{
+        const user = req.user;
+        const recipe = await Recipe.findOne({_id: req.params.id});
+        const recipeId = recipe._id;
+        const alreadyPresent = await Favorite.findOne({userId: user._id, recipeId: recipeId});
+        if(alreadyPresent){
+            throw new Error('Recipe Already Favorite');
+        }
+        const favorite = new Favorite({userId: user._id, recipeId: recipeId});
+        await favorite.save();
+        res.send(favorite);
+    } catch(err){
+        res.status(400).send(err.message);
+    }
+})
+
 
 module.exports = router;
